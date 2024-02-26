0f273d9ef74becb7e475c9f3112db0ffe94c15fa
Avick Saha <70565523+avick-saha@users.noreply.github.com>
2024-02-26 19:34:18 +0530

    Update ProductDetails.js
    to show quantity on product details page...

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -138,7 +138,7 @@ const ProductDetails = ({ match }) => {
                 <div className="detailsBlock-3-1">
                   <div className="detailsBlock-3-1-1">
                     <button onClick={decreaseQuantity}>-</button>
-                    <input readOnly type="number" value={quantity} />
+                    <span id="quantity">{quantity}</span>
                     <button onClick={increaseQuantity}>+</button>
                   </div>
                   <button
